57c6f4b3224bd77cb7eead2390d798446a1e2753
arb8020 <arbeightytwenty@gmail.com>
2025-11-10 09:03:59 -0800

    Update gpu_ranks to [3] for testing on different GPU

--- dev/integration_training/configs/01_debug_sft_rl.py
+++ dev/integration_training/configs/01_debug_sft_rl.py
@@ -27,7 +27,7 @@ from base_config import (
 # Following qwen3_next pattern: configs/01_baseline.py
 config = Config(
     target=TargetConfig(
-        gpu_ranks=[0],  # Single GPU for debug (machine only has GPUs 0-3)
+        gpu_ranks=[3],  # Single GPU for debug (machine has GPUs 0-3)
         device_type="cuda",  # cuda|cpu|mps
     ),
     model=ModelConfig(
