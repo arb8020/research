ed98ca9367105dafdb555f2eb9c83b60dca15670
arb8020 <arbeightytwenty@gmail.com>
2026-02-18 16:32:51 -0800

    fix: install transformers>=5.0 on remote to fix 404 bug

--- rollouts/rollouts/run.py
+++ rollouts/rollouts/run.py
@@ -193,7 +193,10 @@ async def _deploy_and_submit(
         ),
         (
             "Installing ML packages",
-            "~/.local/bin/uv pip install torch transformers datasets accelerate sglang[all] curl_cffi peft",
+            # Override transformers to 5.x to fix list_repo_templates 404 bug
+            # SGLang pins transformers==4.57.1 which has this bug
+            # See: https://github.com/huggingface/transformers/issues/41813
+            "~/.local/bin/uv pip install torch 'transformers>=5.0.0' datasets accelerate sglang[all] curl_cffi peft",
         ),
     ]
 
