796a3118a8f379d99af3fc98711010b8591bccca
arb8020 <arbeightytwenty@gmail.com>
2026-03-13 23:55:10 -0700

    Use param group for Megatron scheduler lr

--- rollouts/rollouts/training/backends/megatron_backend.py
+++ rollouts/rollouts/training/backends/megatron_backend.py
@@ -291,7 +291,8 @@ class MegatronTrainingBackend:
         # Scheduler step
         if self.opt_param_scheduler is not None:
             self.opt_param_scheduler.step(increment=self.config.global_batch_size)
-            lr = self.opt_param_scheduler.get_lr()
+            primary_group = self.optimizer.param_groups[0]
+            lr = self.opt_param_scheduler.get_lr(primary_group)
         else:
             lr = self.optimizer.param_groups[0].get("lr", 0.0)
 
