09a24cc245db57b999c248fb38f40f2e9007ac6d
arb8020 <arbeightytwenty@gmail.com>
2026-03-14 17:44:45 -0700

    fix: shim argparse for patched vllm server

--- rollouts/rollouts/training/vllm_qed_server.py
+++ rollouts/rollouts/training/vllm_qed_server.py
@@ -221,6 +221,19 @@ def main() -> None:
     import uvloop
     from vllm.entrypoints.openai.cli_args import make_arg_parser, validate_parsed_serve_args
 
+    original_boolean_optional_init = argparse.BooleanOptionalAction.__init__
+
+    def _patched_boolean_optional_init(
+        self: Any,
+        option_strings: Any,
+        dest: Any,
+        **kwargs: Any,
+    ) -> None:
+        kwargs.pop("deprecated", None)
+        original_boolean_optional_init(self, option_strings, dest, **kwargs)
+
+    argparse.BooleanOptionalAction.__init__ = _patched_boolean_optional_init
+
     parser = argparse.ArgumentParser(description="rollouts patched vLLM OpenAI server")
     parser = make_arg_parser(parser)
     args = parser.parse_args()
